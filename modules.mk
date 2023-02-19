385196490192d576a7fbd1e3520e33590ebe0cad
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-02-19 22:03:53 +0300

    Updated build scripts and dependencies

--- modules.mk
+++ modules.mk
@@ -157,14 +157,6 @@ LIBJACK_VERSION            := system
 LIBJACK_NAME               := jack
 LIBJACK_TYPE               := pkg
 
-LIBLADSPA_VERSION          := system
-LIBLADSPA_NAME             := ladspa
-LIBLADSPA_TYPE             := opt
-
-LIBLV2_VERSION             := system
-LIBLV2_NAME                := lv2
-LIBLV2_TYPE                := pkg
-
 LIBMSACM_VERSION           := system
 LIBMSACM_NAME              := libmsacm
 LIBMSACM_TYPE              := opt
